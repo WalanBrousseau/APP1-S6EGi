52633e85e96575b150daef42889dbcb4c1ad54a2
cusj3102 <65480663+JeremyCusson@users.noreply.github.com>
2024-05-06 09:50:43 -0400

    Update ftdt_yee.py

--- gif643-proto-E24/waveprop/ftdt_yee.py
+++ gif643-proto-E24/waveprop/ftdt_yee.py
@@ -1,48 +1,50 @@
 # GPL3, Copyright (c) Max Hofheinz, UdeS, 2021
 
-import numpy, fiddle
+import fiddle
+import numpy
+from subprocess import Popen, PIPE
+import mmap
+import time
 
-def curl_E(E):
-    curl_E = numpy.zeros(E.shape)
-    curl_E[:, :-1, :, 0] += E[:, 1:, :, 2] - E[:, :-1, :, 2]
-    curl_E[:, :, :-1, 0] -= E[:, :, 1:, 1] - E[:, :, :-1, 1]
+FNAME        = "GIF642-labo-shm1"
+FNAME2       = "GIF642-labo-shm2"
+MATRIX_SIZE = 100
 
-    curl_E[:, :, :-1, 1] += E[:, :, 1:, 0] - E[:, :, :-1, 0]
-    curl_E[:-1, :, :, 1] -= E[1:, :, :, 2] - E[:-1, :, :, 2]
+def subp():
+    subproc = Popen(["./ftdt_yee", FNAME,FNAME2], stdin=PIPE, stdout=PIPE)
+    return subproc
 
-    curl_E[:-1, :, :, 2] += E[1:, :, :, 1] - E[:-1, :, :, 1]
-    curl_E[:, :-1, :, 2] -= E[:, 1:, :, 0] - E[:, :-1, :, 0]
-    return curl_E
-
-def curl_H(H):
-    curl_H = numpy.zeros(H.shape)
-
-    curl_H[:,1:,:,0] += H[:,1:,:,2] - H[:,:-1,:,2]
-    curl_H[:,:,1:,0] -= H[:,:,1:,1] - H[:,:,:-1,1]
-
-    curl_H[:,:,1:,1] += H[:,:,1:,0] - H[:,:,:-1,0]
-    curl_H[1:,:,:,1] -= H[1:,:,:,2] - H[:-1,:,:,2]
-
-    curl_H[1:,:,:,2] += H[1:,:,:,1] - H[:-1,:,:,1]
-    curl_H[:,1:,:,2] -= H[:,1:,:,0] - H[:,:-1,:,0]
-    return curl_H
+def signal_and_wait(subproc):
+    subproc.stdin.write("START\n".encode())
+    subproc.stdin.flush()                   # Nécessaire pour vider le tampon de sortie
+    res = subproc.stdout.readline()
+    #print(res)
 
 
-def timestep(E, H, courant_number, source_pos, source_val):
-    E += courant_number * curl_H(H)
-    E[source_pos] += source_val
-    H -= courant_number * curl_E(E)
-    return E, H
-
+# Lancement de l'exécutable associé
+# NOTE: suppose que l'exécutable est dans le même dossier que celui en cours (normalement build/)
+subproc = subp()
+# Envoi d'une ligne sur l'entrée du sous-processus et attend un retour pour signaler que
+# nous sommes prêts à passer à la prochaine étape. 
+signal_and_wait(subproc)
+shm_f = open(FNAME, "r+b")
+shm_f2 = open(FNAME2, "r+b") 
+shm_mm = mmap.mmap(shm_f.fileno(), 0)
+shm_mm2 = mmap.mmap(shm_f2.fileno(), 0)
+shared_matrix = numpy.ndarray(shape=(MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, 3), dtype=numpy.float64, buffer=shm_mm)
+shared_matrix2 = numpy.ndarray(shape=(MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE, 3), dtype=numpy.float64, buffer=shm_mm2)
+shared_matrix[:] = numpy.zeros(shape=shared_matrix.shape)
+shared_matrix2[:] = numpy.zeros(shape=shared_matrix2.shape)
 
 class WaveEquation:
     def __init__(self, s, courant_number, source):
         s = s + (3,)
-        self.E = numpy.zeros(s)
-        self.H = numpy.zeros(s)
+        self.E = shared_matrix
+        self.H = shared_matrix2
         self.courant_number = courant_number
         self.source = source
         self.index = 0
+
 
     def __call__(self, figure, field_component, slice, slice_index, initial=False):
         if field_component < 3:
@@ -56,8 +58,7 @@ class WaveEquation:
             field = field[:, slice_index, :, field_component]
         elif slice == 2:
             field = field[:, :, slice_index, field_component]
-        source_pos, source_index = source(self.index)
-        self.E, self.H = timestep(self.E, self.H, self.courant_number, source_pos, source_index)
+        signal_and_wait(subproc)    
 
         if initial:
             axes = figure.add_subplot(111)
@@ -67,17 +68,19 @@ class WaveEquation:
         self.index += 1
 
 
-if __name__ == "__main__":
-    n = 100
-    r = 0.01
-    l = 30
+n = MATRIX_SIZE
+r = 0.01
+l = 30
 
 
-    def source(index):
-        return ([n // 3], [n // 3], [n // 2],[0]), 0.1*numpy.sin(0.1 * index)
+def source(index):
+    return ([n // 3], [n // 3], [n // 2],[0]), 0.1*numpy.sin(0.1 * index)
 
+w = WaveEquation((n, n, n), 0.1, source)
+fiddle.fiddle(w, [('field component',{'Ex':0,'Ey':1,'Ez':2, 'Hx':3,'Hy':4,'Hz':5}),('slice',{'XY':2,'YZ':0,'XZ':1}),('slice index',0,n-1,n//2,1)], update_interval=0.01)
 
-    w = WaveEquation((n, n, n), 0.1, source)
-    fiddle.fiddle(w, [('field component',{'Ex':0,'Ey':1,'Ez':2, 'Hx':3,'Hy':4,'Hz':5}),('slice',{'XY':2,'YZ':0,'XZ':1}),('slice index',0,n-1,n//2,1)], update_interval=0.01)
+print("PY:  Done")
 
-
+subproc.kill()
+shm_mm.close() 
+shm_mm2.close() 
